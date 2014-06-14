968c47fdc8e178dbfaaf37e473f1add1b1ed5c85
shihsan <shihsan8648@yahoo.com.tw>
2014-06-14 14:59:50 +0800

    bobo modify

--- app.js
+++ app.js
@@ -18,34 +18,35 @@
 			// query.limit(limit); 
 			// query.skip(skip);
 			query.descending("updateAt"); 
-
 			query.find({success: function(results){
-
 				var objList = results.map(function(e){ return e.toJSON() });
 				console.log(objList); 
 				document.getElementById('content').innerHTML = templates.indexView(objList);
 				query.limit(0);
+
+
+				var query2 = new Parse.Query(Event);
+				query2.descending("count"); 
+				query2.find({success: function(results){
+					var objList = results.map(function(e){ return e.toJSON() });
+					console.log(objList); 
+					document.getElementById('content').innerHTML += templates.indexView_2(objList);
+					query2.limit(0);
+
+
+					var query3 = new Parse.Query(Event);
+					query3.descending("share_count"); 
+					query3.find({success: function(results){
+						var objList = results.map(function(e){ return e.toJSON() });
+						console.log(objList); 
+						document.getElementById('content').innerHTML += templates.indexView_3(objList);
+						query3.limit(0);
+					}});
+				}});
 			}});
 
-			query.descending("count"); 
 
-			query.find({success: function(results){
-
-				var objList = results.map(function(e){ return e.toJSON() });
-				console.log(objList); 
-				document.getElementById('content').innerHTML += templates.indexView_2(objList);
-				query.limit(0);
-			}});
-
-			query.descending("share_count"); 
-
-			query.find({success: function(results){
-
-				var objList = results.map(function(e){ return e.toJSON() });
-				console.log(objList); 
-				document.getElementById('content').innerHTML += templates.indexView_3(objList);
-				query.limit(0);
-			}});
+			
 
 		},
 		eventView:function(){
@@ -378,7 +379,7 @@
 				query.limit(0);
 			}});
 
-		},
+		}
 	};
 
 	var r=Parse.Router.extend({
@@ -397,7 +398,7 @@
 			"care": 		"care_event", 
 			"activity": 	"activity_event", 
 			"camp": 		"camp_event", 
-			"other": 		"other_event", 
+			"other": 		"other_event" 
 		},
 		indexView: 		volunteer.indexView,
 		eventView: 		volunteer.eventView,
@@ -413,7 +414,7 @@
 		care_event: 	volunteer.care_event, 
 		activity_event: volunteer.activity_event, 
 		camp_event: 	volunteer.camp_event, 
-		other_event: 	volunteer.other_event, 
+		other_event: 	volunteer.other_event
 	});
 
 	// Initialize the App
@@ -479,6 +480,50 @@ function FacebookLogin(){
 			}
 		},{scope:"user_likes,user_photos,publish_actions"})
 	}
+}
+function arise(){
+	  $( "#dialog-form" ).dialog({
+      autoOpen: false,
+      height: 300,
+      width: 350,
+      modal: true,
+      buttons: {
+        "Create an account": function() {
+          var bValid = true;
+          allFields.removeClass( "ui-state-error" );
+ 
+          bValid = bValid && checkLength( name, "username", 3, 16 );
+          bValid = bValid && checkLength( email, "email", 6, 80 );
+          bValid = bValid && checkLength( password, "password", 5, 16 );
+ 
+          bValid = bValid && checkRegexp( name, /^[a-z]([0-9a-z_])+$/i, "Username may consist of a-z, 0-9, underscores, begin with a letter." );
+          // From jquery.validate.js (by joern), contributed by Scott Gonzalez: http://projects.scottsplayground.com/email_address_validation/
+          bValid = bValid && checkRegexp( email, /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i, "eg. ui@jquery.com" );
+          bValid = bValid && checkRegexp( password, /^([0-9a-zA-Z])+$/, "Password field only allow : a-z 0-9" );
+ 
+          if ( bValid ) {
+            $( "#users tbody" ).append( "<tr>" +
+              "<td>" + name.val() + "</td>" +
+              "<td>" + email.val() + "</td>" +
+              "<td>" + password.val() + "</td>" +
+            "</tr>" );
+            $( this ).dialog( "close" );
+          }
+        },
+        Cancel: function() {
+          $( this ).dialog( "close" );
+        }
+      },
+      close: function() {
+        allFields.val( "" ).removeClass( "ui-state-error" );
+      }
+    });
+ 
+    $( "#arise_event" )
+      .button()
+      .click(function() {
+        $( "#dialog-form" ).dialog( "open" );
+      });
 }
 
 
