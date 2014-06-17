8305b129391a151dcc73a60b92440c926a3a1a90
shihsan <shihsan8648@yahoo.com.tw>
2014-06-18 00:48:55 +0800

    fb likes test

--- app.js
+++ app.js
@@ -137,7 +137,10 @@
 				query.get(eventdetail_id, { 
 					success: function(eventdetail){
 						document.getElementById('content').innerHTML = templates.EventDetailView(eventdetail.toJSON());
-						// document.getElementById('fb_likes').innerHTML =
+						var url = 'http://www.google.com.tw/';
+						var encode_url = encodeURIComponent(url);
+						document.getElementById('fb_likes').innerHTML =
+						'<iframe src="//www.facebook.com/plugins/like.php?href='.encode_url.'width&amp;layout=button_count&amp;action=like&amp;show_faces=true&amp;share=true&amp;height=21&amp;appId=299735220203958" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:21px;" allowTransparency="true"></iframe>';
 						// '<iframe src="//www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Fpageaffairs&send=false&layout=button_count&width=450&show_faces=false&action=like&colorscheme=light&font&height=21" scrolling="no" frameborder="0" style="border:none;overflow:hidden;width:450px;height:21px;" allowTransparency="true"></iframe>';
 
 
